fe3e753d04c5c02568da58dbb99e3bc2e6e0f21b
Elliot <elliot.gustafsson03@gmail.com>
2024-06-10 09:46:45 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import './css/App.css'
+
+function App() {
+    return <></>
+}
+
+export default App
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import { render } from 'solid-js/web'
+
+import './css/index.css'
+import App from './App'
+
+const root = document.getElementById('root')
+
+render(() => <App />, root!)
